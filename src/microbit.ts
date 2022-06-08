4a3714585f454428cdb12c52f99500b94bba77ae
thegecko <rob.moran@arm.com>
2022-06-08 11:06:32 +0100

    Tweaks

--- src/microbit.ts
+++ src/microbit.ts
@@ -1,10 +1,11 @@
-import { CortexM } from 'dapjs';
+import { CortexM, WebUSB } from 'dapjs';
 
 // https://infocenter.nordicsemi.com/index.jsp?topic=%2Fcom.nordic.infocenter.nrf52832.ps.v1.1%2Fficr.html
-const DEVICE_ID_1 = 0x10000064; // FICR (0x10000000) + DEVICE_ID (0x064)
+const FICR = 0x10000000;
+const DEVICE_ID_1 = 0x064;
+
 const MICROBIT_NAME_LENGTH = 5;
 const MICROBIT_NAME_CODE_LETTERS = 5;
-
 const CODEBOOK = [
     ['z', 'v', 'g', 'p', 't'],
     ['u', 'o', 'i', 'e', 'a'],
@@ -13,19 +14,20 @@ const CODEBOOK = [
     ['z', 'v', 'g', 'p', 't']
 ]
 
-export class MicroBit extends CortexM {
+class MicroBit extends CortexM {
+
     // https://github.com/lancaster-university/codal-microbit-v2/blob/e7236cd69c01a5e803fe3697fd2ef994808794e9/source/MicroBitDevice.cpp#L130
-    async microbitFriendlyName(): Promise<string> {
+    public async microbitFriendlyName(length = MICROBIT_NAME_LENGTH): Promise<string> {
         // Microbit only uses MSB of serial number
-        let msb = await this.readMem32(DEVICE_ID_1);
+        let serial = await this.readMem32(FICR + DEVICE_ID_1);
 
         let d = MICROBIT_NAME_CODE_LETTERS;
         let ld = 1;
         let name = '';
 
-        for (let i = 0; i < MICROBIT_NAME_LENGTH; i++) {
-            const h = Math.floor((msb % d) / ld);
-            msb -= h;
+        for (let i = 0; i < length; i++) {
+            const h = Math.floor((serial % d) / ld);
+            serial -= h;
             d *= MICROBIT_NAME_CODE_LETTERS;
             ld *= MICROBIT_NAME_CODE_LETTERS;
             name = CODEBOOK[i][h] + name;
@@ -34,3 +36,17 @@ export class MicroBit extends CortexM {
         return name;
     }
 }
+
+export const getFriendlyName = async (usbDevice: USBDevice): Promise<string | undefined> => {
+    const transport = new WebUSB(usbDevice);
+    const processor = new MicroBit(transport);
+
+    try {
+        await processor.connect();
+        return processor.microbitFriendlyName();
+    } catch {
+        return undefined;
+    } finally {
+        await processor.disconnect();
+    }
+};
